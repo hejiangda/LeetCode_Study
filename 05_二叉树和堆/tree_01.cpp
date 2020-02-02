d3c94e72bb4aa14a17892912f21b63598e920a30
hejiangda <hejiangda@163.com>
2020-02-03 01:11:18 +0800

    bst

---
+++ 05_二叉树和堆/tree_01.cpp
@@ -0,0 +1,84 @@
+// - 实现一个二叉查找树，并且支持插入、删除、查找操作
+#include<bits/stdc++.h>
+
+using namespace std;
+class Tree{
+public:
+    struct Node{
+        int data;
+        Node* left;
+        Node* right;
+        Node(int x){
+            data=x;
+            left=nullptr;
+            right=nullptr;
+        }
+    };
+    Tree(){
+        T=nullptr;
+    }
+    Tree& insert(int x){
+        insert(x,T);
+        return *this;
+    }
+    void insert(int x,Node*& p){
+        if(p==nullptr){
+            p=new Node(x);
+            return ;
+        }
+        if(p->data>x){
+            return insert(x,p->left);
+        }
+        else return insert(x,p->right);
+    }
+    void inorderTravel(){
+        inorderTravel(T);
+        cout<<endl;
+    }
+    void inorderTravel(Node* p){
+        if(p==nullptr)
+            return;
+        inorderTravel(p->left);
+        cout<<p->data<<" ";
+        inorderTravel(p->right);
+    }
+    Tree& del(int x){
+        del(x,T);
+        return *this;
+    }
+    void del(int x,Node*& p){
+        if(p==nullptr)
+            return;
+        if(p->data==x){
+            if(p->right){
+                Node * tmp=p->right;
+                while(tmp->left)
+                    tmp=tmp->left;
+                tmp->left=p->left;
+                tmp=p->right;
+                delete p;
+                p=tmp;
+            }
+            else {
+                Node * tmp=p->left;
+                delete p;
+                p=tmp;
+            }
+            return;
+        }
+        else if(p->data>x)
+            del(x,p->left);
+        else 
+            del(x,p->right);
+    }
+private:
+    Node* T;
+};
+int main(){
+    Tree bst;
+    bst.insert(4).insert(2).insert(1).insert(3).insert(5);
+    bst.inorderTravel();
+    bst.del(5).del(1);
+    bst.inorderTravel();
+    return 0;
+}
