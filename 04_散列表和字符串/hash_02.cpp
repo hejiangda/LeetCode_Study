8f2a2f786237f7bf4d0be2a43f9bd17e28caf039
hejiangda <hejiangda@163.com>
2020-02-04 13:30:09 +0800

    链表LRU

--- 04_散列表和字符串/hash_02.cpp
+++ 04_散列表和字符串/hash_02.cpp
@@ -1,8 +1,44 @@
 // - 实现一个 LRU 缓存淘汰算法
+
 #include <bits/stdc++.h>
 
 using namespace std;
-int main(){
+// 链表版
+class LRU{
+public:
+    bool find(int x){
+        for(int c:data){
+            if(c==x){
+                data.remove(x);
+                data.push_front(x);
+            }
+        }
+        return false;
+    }
+    LRU& insert(int x){
+        data.push_back(x);
+        return *this;
+    }
+    LRU& print(){
+        cout<<"LRU:";
+        for(int x:data){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+        return *this;
+    }
     
+private:
+    list<int> data;
+};
+
+
+int main(){
+    LRU lru;
+    lru.insert(1).insert(2).insert(3).insert(4).insert(5).insert(6).insert(7);
+    lru.print();
+    lru.find(3);
+    lru.find(6);
+    lru.print();
     return 0;
 }
