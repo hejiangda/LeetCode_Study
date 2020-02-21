1d9ca718a321ee0cd4e5ce7f2cbc5c6d8f8e4978
hejiangda <hejiangda@163.com>
2020-02-21 20:18:08 +0800

    三次反转

--- LeetCode/189.旋转数组.cpp
+++ LeetCode/189.旋转数组.cpp
@@ -9,23 +9,15 @@ using namespace std;
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k = k % nums.size();
-        int cnt=0;
-        for (int start = 0; start < k and cnt<nums.size(); start++) {
-            int current = start;
-            int prev = nums[start];
-            do {
-                cnt++;
-                int next = (current+k)%nums.size();
-                int tmp = nums[next];
-                nums[next] = prev;
-                prev = tmp;
-                current = next;
-            }while(current != start);
-        }
+        k=k%nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
+    
 };
 // @lc code=end
 // 1. 暴力法依次移动数据
 // 2. 计算好每个数字移动后的位置，然后把数据填入临时数组，最后把数据复制到原数组
-// 3. 环替换法，这个我昨天想到了，但是一直卡在了循环回到起点上。
+// 3. 环替换法，这个我想到了，但是一直卡在了循环回到起点上。
+// 4. 反转法，整体反转一次，在k位置前后分别反转
