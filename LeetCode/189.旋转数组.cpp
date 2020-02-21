be41af028016e734a19647fc440a5bc816784c6d
hejiangda <hejiangda@163.com>
2020-02-21 20:09:20 +0800

    环替换，解决卡在循环起点的问题

--- LeetCode/189.旋转数组.cpp
+++ LeetCode/189.旋转数组.cpp
@@ -9,15 +9,23 @@ using namespace std;
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> tmp(nums.size());
-        for(int i=0;i<nums.size();i++){
-            tmp[(i+k)%nums.size()]=nums[i];
-        }
-        for(int i=0;i<nums.size();i++){
-            nums[i]=tmp[i];
+        k = k % nums.size();
+        int cnt=0;
+        for (int start = 0; start < k and cnt<nums.size(); start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                cnt++;
+                int next = (current+k)%nums.size();
+                int tmp = nums[next];
+                nums[next] = prev;
+                prev = tmp;
+                current = next;
+            }while(current != start);
         }
     }
 };
 // @lc code=end
 // 1. 暴力法依次移动数据
 // 2. 计算好每个数字移动后的位置，然后把数据填入临时数组，最后把数据复制到原数组
+// 3. 环替换法，这个我昨天想到了，但是一直卡在了循环回到起点上。
