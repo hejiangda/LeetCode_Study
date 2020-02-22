fecdbdcb8a20bcf4f7d008884cf75cf9fbad8b17
hejiangda <hejiangda@163.com>
2020-02-22 15:27:06 +0800

    迭代法交换链表结点

--- LeetCode/24.两两交换链表中的节点.cpp
+++ LeetCode/24.两两交换链表中的节点.cpp
@@ -16,20 +16,30 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        if(head==nullptr or head->next==nullptr)return head;
-        ListNode* firstNode=head;
-        ListNode* secondNode=head->next;
+        ListNode dummy(-1);
+        dummy.next=head;
+        ListNode* prevNode=&dummy;
 
-        firstNode->next=swapPairs(secondNode->next);
-        secondNode->next=firstNode;
+        while(head and head->next) {
+            ListNode* firstNode=head;
+            ListNode* secondNode=head->next;
 
-        return secondNode;
+            firstNode->next=secondNode->next;
+            secondNode->next=firstNode;
+            prevNode->next=secondNode;
+
+            prevNode=firstNode;
+            head=firstNode->next;
+        }
+        return dummy.next;
     }
     // 1. 递归法，swapPairs返回的是交换后结点的头
     // 第一个结点的下一个结点是第三个结点交换后的结点
     // 第二个结点的下一个结点是第一个结点
     // 第二个结点为头返回
-    // 2. 
+    // 2. 迭代法，核心语句 firstNode->next=secondNode->next;
+    // secondNode->next=firstNode;
+    // 需要有个临时结点保存第一个结点，临时指针保存前一个结点
 };
 // @lc code=end
 
