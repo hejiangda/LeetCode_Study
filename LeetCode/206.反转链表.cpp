1054999a83df1ee8509369993ab39f543fb7bad7
hejiangda <hejiangda@163.com>
2020-02-22 12:58:18 +0800

    递归法

---
+++ LeetCode/206.反转链表.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ *
+ * [206] 反转链表
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+//   struct ListNode {
+//       int val;
+//       ListNode *next;
+//       ListNode(int x) : val(x), next(NULL) {}
+//   };
+class Solution {
+public:
+    ListNode* h;
+    ListNode* reverseL(ListNode* head) {
+        if(head->next) {
+            reverseL(head->next)->next=head;
+        }else {
+            h=head;
+        }
+        return head;
+    }
+    ListNode* reverseList(ListNode* head) {      
+        if(head==nullptr)return nullptr;  
+        reverseL(head)->next=nullptr;
+        return h;
+    }
+    // 1. 暴力法，依次遍历找到每个结点把他们一个一个串起来，n^2
+    // 2. 三指针，一个指向当前结点，一个指向前一个结点，一个保存后一个结点的值，
+    // 依次把当前结点的下一个结点指向前一个结点
+    // 3. 记忆化递归解决
+};
+// @lc code=end
+
