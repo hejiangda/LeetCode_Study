fcc7b0b728f8dbd2771f206b463b498486016e51
hejiangda <hejiangda@163.com>
2020-02-24 21:01:13 +0800

    用递归法解决后缀表达式

--- LeetCode/150.逆波兰表达式求值.cpp
+++ LeetCode/150.逆波兰表达式求值.cpp
@@ -9,32 +9,27 @@ using namespace std;
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        deque<int> st;
-        for(auto x:tokens) {
-            if(x=="+"){
-                int b=st.back();st.pop_back();
-                int a=st.back();st.pop_back();
-                st.push_back(a+b);
-            }else if(x=="-"){
-                int b=st.back();st.pop_back();
-                int a=st.back();st.pop_back();
-                st.push_back(a-b);
-            }else if(x=="*"){
-                int b=st.back();st.pop_back();
-                int a=st.back();st.pop_back();
-                st.push_back(a*b);
-            }else if(x=="/"){
-                int b=st.back();st.pop_back();
-                int a=st.back();st.pop_back();
-                st.push_back(a/b);
-            }else {
-                st.push_back(stoi(x));
-            }
-        }
-        return st.back();
+        int index=tokens.size()-1;
+        return eval(tokens,index);
+    }
+    int eval(vector<string>& tokens,int& index) {
+        string tmp=tokens[index--];
+        if(tmp=="+") {
+            return eval(tokens,index)+eval(tokens,index);
+        }else if(tmp=="-") {
+            int t=eval(tokens,index);
+            return eval(tokens,index)-t;
+        }else if(tmp=="*") {
+            return eval(tokens,index)*eval(tokens,index);
+        }else if(tmp=="/") {
+            int t=eval(tokens,index);
+            return eval(tokens,index)/t;
+        }else return stoi(tmp);
     }
     // 1. 后缀表达式求值：遇到数字存到栈中，遇到一个运算符就弹出两个数字，
     // 做相应运算，之后把结果再压栈，
+    // 2. 递归法：从后向前，先得到运算符，之后两个操作数分别是前一个和前前一个求到的值
+    // 也就是说该函数返回后缀表示法得到的值，将两个操作数分别当成已知的
 };
 // @lc code=end
 
